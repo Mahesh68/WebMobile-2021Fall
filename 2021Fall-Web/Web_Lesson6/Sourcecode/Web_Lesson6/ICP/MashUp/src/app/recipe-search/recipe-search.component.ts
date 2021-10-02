d674c8b841863c879b4fba4b488a15ef1cad099c
Mahesh68 <43518488+Mahesh68@users.noreply.github.com>
2021-10-02 18:30:16 -0500

    initial commit

--- 2021Fall-Web/Web_Lesson6/Sourcecode/Web_Lesson6/ICP/MashUp/src/app/recipe-search/recipe-search.component.ts
+++ 2021Fall-Web/Web_Lesson6/Sourcecode/Web_Lesson6/ICP/MashUp/src/app/recipe-search/recipe-search.component.ts
@@ -1,47 +1,67 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
-import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, TemplateRef } from "@angular/core";
+import { BsModalService, BsModalRef } from "ngx-bootstrap/modal";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-recipe-search',
-  templateUrl: './recipe-search.component.html',
-  styleUrls: ['./recipe-search.component.css']
+  selector: "app-recipe-search",
+  templateUrl: "./recipe-search.component.html",
+  styleUrls: ["./recipe-search.component.css"],
 })
 export class RecipeSearchComponent implements OnInit {
+  // initializing values
   public recipeName;
   public recipeList = [];
   public ingredientList = [];
   public selectedRecipe;
   public processSteps = [];
   modalRef: BsModalRef;
-  constructor(private modalService: BsModalService, private http:HttpClient) {}
+  constructor(private modalService: BsModalService, private http: HttpClient) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
+  // this method will be called on click of get recipes
   getRecipes() {
-    this.http.get(`https://api.spoonacular.com/recipes/complexSearch?query=${this.recipeName}&apiKey=79fb28cac7b943268ebd5d8edb109a43`).subscribe(data => {
-      const recipes = data['results'].slice(0, 4);
-      this.recipeList = recipes;
-    })
+    /* 
+    ajax call for making http call for recipes
+    Observable will be cold untill it gets subscribed
+
+    Streams of data pushing into the recipes variable
+    */
+    this.http
+      .get(
+        `https://api.spoonacular.com/recipes/complexSearch?query=${this.recipeName}&apiKey=7330649a12664a7caed7dbb1f169085b`
+      )
+      .subscribe((data) => {
+        const recipes = data["results"].slice(0, 4);
+        this.recipeList = recipes;
+      });
   }
 
+  /*
+   * When user clicks on details of that ingredient, modal will be opened with the details of that recipe
+   */
   openDetails(recipe, template: TemplateRef<any>) {
     this.selectedRecipe = recipe;
     this.ingredientList = [];
-    this.http.get(`https://api.spoonacular.com/recipes/${recipe.id}/analyzedInstructions?apiKey=79fb28cac7b943268ebd5d8edb109a43`).subscribe(data => {
-      const steps = data[0]['steps'];
-      console.log(data,steps);
-      steps.map(step => {
-        const ingredient = step.ingredients.map(ing => ing.name)
-        this.ingredientList.push(...ingredient);
-        const process = {
-          number: step.number,
-          desc: step.step
-        }
-        this.processSteps.push(process);
+    this.http
+      .get(
+        `https://api.spoonacular.com/recipes/${recipe.id}/analyzedInstructions?apiKey=7330649a12664a7caed7dbb1f169085b`
+      )
+      .subscribe((data) => {
+        const steps = data[0]["steps"];
+        console.log(data, steps);
+        steps.map((step) => {
+          const ingredient = step.ingredients.map((ing) => ing.name);
+          this.ingredientList.push(...ingredient);
+          const process = {
+            number: step.number,
+            desc: step.step,
+          };
+          this.processSteps.push(process);
+        });
       });
-    })
-    this.modalRef = this.modalService.show(template, {class:'recipe-modal'});
+
+    // After making the user data ready modal is shown
+    this.modalRef = this.modalService.show(template, { class: "recipe-modal" });
   }
 }
--- 2021Fall-Web/Web_Lesson6/Sourcecode/Web_Lesson6/ICP/MashUp/src/app/restaurant-search/restaurant-search.component.ts
+++ 2021Fall-Web/Web_Lesson6/Sourcecode/Web_Lesson6/ICP/MashUp/src/app/restaurant-search/restaurant-search.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
-import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
-import { HttpClient } from '@angular/common/http';
-import { environment } from './../../environments/environment';
+import { Component, OnInit, TemplateRef } from "@angular/core";
+import { BsModalService, BsModalRef } from "ngx-bootstrap/modal";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "./../../environments/environment";
 
 @Component({
-  selector: 'app-restaurant-search',
-  templateUrl: './restaurant-search.component.html',
-  styleUrls: ['./restaurant-search.component.css']
+  selector: "app-restaurant-search",
+  templateUrl: "./restaurant-search.component.html",
+  styleUrls: ["./restaurant-search.component.css"],
 })
 export class RestaurantSearchComponent implements OnInit {
   public selectedRestaurant;
@@ -14,10 +14,10 @@ export class RestaurantSearchComponent implements OnInit {
   public restaurantsList = [];
   public address: any;
   public currentLocation: any;
-  constructor(private modalService: BsModalService, private http: HttpClient) { }
+  constructor(private modalService: BsModalService, private http: HttpClient) {}
 
   ngOnInit() {
-    this.getPosition().then(res => {
+    this.getPosition().then((res) => {
       this.currentLocation = res;
       this.getNearByRestaurants();
     });
@@ -25,83 +25,59 @@ export class RestaurantSearchComponent implements OnInit {
 
   getNearByRestaurants() {
     this.restaurantsList = [];
-    this.http.get(`https://api.foursquare.com/v2/venues/search?ll=${this.currentLocation.lat},${this.currentLocation.lng}&categoryId=4d4b7105d754a06374d81259&client_id=${environment.FOURSQUARE_CLIENT_ID}&client_secret=${environment.FOURSQUARE_CLIENT_SECRET}&v=20210224`).subscribe(response => {
-      const venues = response['response']['venues'].slice(0, 4);
-      console.log(venues)
-      venues.map(venue => {
-        const obj = {
-          name: venue.name,
-          id:venue.id,
-          address: venue['location']['formattedAddress']
-        }
-        console.log(obj)
-        this.restaurantsList.push(obj);
-      })
-    })
+
+    // using four square API to fetch the data of nearby restaurants
+    this.http
+      .get(
+        `https://api.foursquare.com/v2/venues/search?ll=${this.currentLocation.lat},${this.currentLocation.lng}&categoryId=4d4b7105d754a06374d81259&client_id=${environment.FOURSQUARE_CLIENT_ID}&client_secret=${environment.FOURSQUARE_CLIENT_SECRET}&v=20210224`
+      )
+      .subscribe((response) => {
+        const venues = response["response"]["venues"].slice(0, 4);
+        console.log(venues);
+        venues.map((venue) => {
+          const obj = {
+            name: venue.name,
+            id: venue.id,
+            address: venue["location"]["formattedAddress"],
+          };
+          console.log(obj);
+          this.restaurantsList.push(obj);
+        });
+      });
   }
 
+  // http call is happening here on click of get retaurants method
   getRestaurants() {
     this.restaurantsList = [];
-    this.http.get(`https://api.foursquare.com/v2/venues/search?near=${this.address}&categoryId=4d4b7105d754a06374d81259&client_id=${environment.FOURSQUARE_CLIENT_ID}&client_secret=${environment.FOURSQUARE_CLIENT_SECRET}&v=20210224`).subscribe(response => {
-      const venues = response['response']['venues'].slice(0, 4);
-      console.log(venues)
-      venues.map(venue => {
-        const obj = {
-          name: venue.name,
-          id:venue.id,
-          address: venue['location']['formattedAddress']
-        }
-        console.log(obj)
-        this.restaurantsList.push(obj);
-      })
-
-    })
-  }
-
-   getPosition() {
-    return new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(resp => {
-          resolve({lng: resp.coords.longitude, lat: resp.coords.latitude});
-        },
-        err => {
-          reject(err);
+    this.http
+      .get(
+        `https://api.foursquare.com/v2/venues/search?near=${this.address}&categoryId=4d4b7105d754a06374d81259&client_id=${environment.FOURSQUARE_CLIENT_ID}&client_secret=${environment.FOURSQUARE_CLIENT_SECRET}&v=20210224`
+      )
+      .subscribe((response) => {
+        const venues = response["response"]["venues"].slice(0, 4);
+        console.log(venues);
+        venues.map((venue) => {
+          const obj = {
+            name: venue.name,
+            id: venue.id,
+            address: venue["location"]["formattedAddress"],
+          };
+          console.log(obj);
+          this.restaurantsList.push(obj);
         });
+      });
+  }
+
+  getPosition() {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        (resp) => {
+          resolve({ lng: resp.coords.longitude, lat: resp.coords.latitude });
+        },
+        (err) => {
+          reject(err);
+        }
+      );
     });
-
   }
-
-  openDetails(restaurant, template: TemplateRef<any>) {
-    this.http.get(`https://api.foursquare.com/v2/venues/${restaurant.id}&client_id=${environment.FOURSQUARE_CLIENT_ID}&client_secret=${environment.FOURSQUARE_CLIENT_SECRET}&v=20210224`).subscribe(data => {
-      console.log(data)
-    })
-    this.selectedRestaurant = {
-      "formatted_address": restaurant.address.join(","),
-      "formatted_phone_number": "(02) 9696 2500",
-      "name": restaurant.name,
-      "reviews" : [
-         {
-            "author_name" : "Robert Ardill",
-            "author_url" : "https://www.google.com/maps/contrib/106422854611155436041/reviews",
-            "language" : "en",
-            "profile_photo_url" : "https://lh3.googleusercontent.com/-T47KxWuAoJU/AAAAAAAAAAI/AAAAAAAAAZo/BDmyI12BZAs/s128-c0x00000000-cc-rp-mo-ba1/photo.jpg",
-            "rating" : 5,
-            "relative_time_description" : "a month ago",
-            "text" : "Awesome offices. Great facilities, location and views. Staff are great hosts",
-            "time" : 1491144016
-        },
-        {
-            "author_name": "Kalain Y",
-            "author_url": "https://www.google.com/maps/contrib/103402089989904997713/reviews",
-            "language": "en",
-            "profile_photo_url": "https://lh3.googleusercontent.com/-X_ybIBiuzjo/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclGA4sAuZP7VLMjv4AidLK07GXpjQ/s128-c0x00000000-cc-rp-mo/photo.jpg",
-            "rating": 2,
-            "relative_time_description": "a month ago",
-            "text": "Why are Yous charging me a monthly fee if I have no subscriptions with yous, charging me $80 whenever Yous feel like it is not ok .",
-            "time": 1611208302
-        },
-      ],
-    }
-    this.modalRef = this.modalService.show(template);
-  }
-
 }
